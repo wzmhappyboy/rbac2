af71eca710b8a3ea2d6d7d9034158b0e9a5d508e
zimingw <940951428@qq.com>
2020-02-09 18:35:25 +0800

    2月9日电

--- src/main/java/com/cisco/rbac/controller/PageController.java
+++ src/main/java/com/cisco/rbac/controller/PageController.java
@@ -1,12 +1,16 @@
 package com.cisco.rbac.controller;
 
 import com.cisco.rbac.entity.RolePermissionRelation;
+import com.cisco.rbac.entity.User;
 import com.cisco.rbac.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.relational.core.sql.In;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+//import com.iteason.anbaoli_vote_system.utils.AjaxResult;
+
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,14 +23,40 @@ public class PageController {
     UserServiceImpl userService;
 
     @ResponseBody
-    @RequestMapping("showpermission")
-    public  ModelAndView showpermission( )
-    {
+    @PostMapping(value = "/login")
+    public Map<String,String> login(@RequestBody Map<String,String> userMap) {
+        int id = Integer.parseInt(userMap.get("id"));
+        String password = userMap.get("password");
+        boolean result = userService.checkUserAndPassword(id, password);
+        if (result) {
+            String token = userService.getToken(userMap.get("id"));
+            Map map = new HashMap<String, String>();
+            map.put("token", token);
+            return map;
+        } else {
+            Map map = new HashMap<String, String>();
+            map.put("erro", "something wrong happern");
+            return map;
+        }
 
+    }
+
+
+
+
+
+
+    @ResponseBody
+    @RequestMapping("showpermission/{id}")
+    public  ModelAndView showpermission(@PathVariable("id") String id )
+    {
+    System.out.println("restful传的参数:"+id);
         ModelAndView modelAndView=new ModelAndView();
         modelAndView.setViewName("showpermission");
         return modelAndView;
     }
+
+
     @ResponseBody
     @RequestMapping("/permissionin")
     public Map<String,Object> showpermissions(@RequestBody String id)
